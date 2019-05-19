f9eaf6f0c707b8ddb96e4c9b5867ed6273e72b8e
ji5485 <ji5485@naver.com>
2019-05-19 20:26:50 +0900

    useInput Advanced

--- src/components/useInput.js
+++ src/components/useInput.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-const Number = styled.div`
+const Text = styled.div`
   font-size: 5rem;
   font-weight: 300;
   text-align: center;
@@ -30,19 +30,68 @@ const Button = styled.div`
   }
 `;
 
-const useInput = () => {
-  const [number, setNumber] = useState(0);
+const Border = styled.div`
+  height: 1px;
+  background: #333;
+  margin: 50px 0;
+`;
 
+const Input = styled.input`
+  width: 300px;
+  padding: 10px;
+  font-size: 1.5rem;
+  font-weight: 300;
+  display: block;
+  margin: 0 auto;
+  outline: none;
+  border: 1.5px solid lightgrey;
+  border-radius: 5px;
+  transition: 0.3s border-color;
+
+  &:focus {
+    border-color: black;
+  }
+`;
+
+// input
+const inputHooks = (initialValue, validator) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [value, setValue] = useState(initialValue);
+
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+
+    let willUpdate = typeof validator === "function" ? validator(value) : true;
+
+    if (willUpdate) setValue(e.target.value);
+  };
+
+  return { value, onChange };
+};
+
+const useInput = () => {
+  // number
+  const [number, setNumber] = useState(0);
   const increment = () => setNumber(number + 1);
   const decrement = () => setNumber(number - 1);
 
+  // input
+  const maxLen = value => value.length <= 10;
+  const name = inputHooks("Mr. ", maxLen);
+
   return (
     <div>
-      <Number>{number}</Number>
+      <Text>{number}</Text>
       <ButtonWrapper>
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
       </ButtonWrapper>
+
+      <Border />
+
+      <Input {...name} name="name" />
     </div>
   );
 };
